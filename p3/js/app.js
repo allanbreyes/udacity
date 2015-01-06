646f0131ffa29fc6f347436eb13f50b4c15c0d36
Allan Reyes <allanbreyes@gmail.com>
2015-01-06 12:22:45 -0600

    add main gameplay functionality

--- p3/js/app.js
+++ p3/js/app.js
@@ -1,46 +1,161 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// global variables
+var difficulty = 100;
+var difficultyStep = 10;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+// common functions
+
+function randomInteger(minimum, maximum) {
+  return Math.floor(Math.random()*(maximum - minimum + 1) + minimum);
 }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+function getRandomSpeed(difficulty) {
+  return randomInteger(0, 100) + difficulty;
+}
+
+function selectRandom(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function checkCollision(object, player) {
+  return (player.x > object.x - object.hitBox.x/2 &&
+          player.x < object.x + object.hitBox.x/2 &&
+          player.y > object.y - object.hitBox.y/2 &&
+          player.y < object.y + object.hitBox.y/2);
+}
+// Actor (super class)
+
+var Actor = function(x, y, sprite) {
+  this.sprite = sprite;
+  this.x = x;
+  this.y = y;
+};
+Actor.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Enemy
+var Enemy = function(x, y, sprite) {
+  sprite = sprite || 'images/enemy-bug.png';
+  Actor.call(this, x, y, sprite);
+  this.speed = getRandomSpeed(difficulty); // units = [px/dt]
+};
+Enemy.prototype = Object.create(Actor.prototype);
+Enemy.prototype.hitBox = {'x': 101, 'y': 83};
+Enemy.prototype.startY = [68, 151, 234];
+Enemy.prototype.constructor = Enemy;
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-}
+  // update position and wrap-around if past edge
+  if (this.x <= (canvas.width + this.hitBox.x/2)) {
+    this.x += this.speed * dt;
+  } else {
+    this.x = -this.hitBox.x;
+    this.y = selectRandom(this.startY);
+    this.speed = getRandomSpeed(difficulty);
+  }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+  // handle collisions with player
+  if (checkCollision(this, player)) {
+    player.reset();
+  }
+};
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player
+var Player = function(x, y, sprite) {
+  sprite = sprite || 'images/char-boy.png';
+  x = x || 200;
+  y = y || 400;
+  Actor.call(this, x, y, sprite);
+};
+Player.prototype = Object.create(Actor.prototype);
+Player.prototype.constructor = Player;
+Player.prototype.update = function() {
+  // process action and move player
+  var stepX = 101;
+  var stepY = 83;
+  switch(this.action) {
+    case 'up':
+      if (this.y > canvas.boundaries.up) {
+        this.y -= stepY;
+      }
+      break;
+    case 'right':
+      if (this.x < canvas.boundaries.right) {
+        this.x += stepX;
+      }
+      break;
+    case 'down':
+      if (this.y < canvas.boundaries.down) {
+        this.y += stepY;
+      }
+      break;
+    case 'left':
+      if (this.x > canvas.boundaries.left) {
+        this.x -= stepX;
+      }
+      break;
+  }
+  // log position
+  if (this.position !== this.x + ',' + this.y) {
+    this.position = this.x + ',' + this.y;
+    console.log(this.position);
+  }
+  // reset action
+  this.action = null;
+
+  // reset player if on goal (water)
+  if (this.y < 25) {
+    this.reset();
+  }
+
+};
+Player.prototype.handleInput = function(e) {
+  this.action = e;
+};
+Player.prototype.reset = function() {
+  this.x = 200;
+  this.y = 400;
+};
+
+// Prize
+var Prize = function(x, y, sprite) {
+  sprite = sprite || 'images/Star.png';
+  x = x || 200;
+  y = y || 68;
+  Actor.call(this, x, y, sprite);
+};
+Prize.prototype = Object.create(Actor.prototype);
+Prize.prototype.hitBox = {'x': 101, 'y': 83};
+Prize.prototype.startX = [-2, 99, 200, 301, 402];
+Prize.prototype.constructor = Prize;
+Prize.prototype.update = function(dt) {
+  // handle collisions with player
+  if (checkCollision(this, player)) {
+    player.reset();
+    this.x = selectRandom(this.startX);
+  }
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [
+  new Enemy(-100, 68),
+  new Enemy(-100, 151),
+  new Enemy(-100, 234)
+];
+var player = new Player();
+var prize = new Prize();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
