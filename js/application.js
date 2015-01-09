2d0b5e482b629051bde870af2fa6d1ffe82e86a5
Allan Reyes <allanbreyes@gmail.com>
2015-01-09 01:01:02 -0600

    add partial functionality (in progress commit)

--- js/application.js
+++ js/application.js
@@ -1,90 +1,297 @@
-var map;
-var center = new google.maps.LatLng(41.878114, -87.629798);
+var meetupApiUrl = 'http://api.meetup.com/2/open_events?status=upcoming&radius=30&category=34&and_text=False&limited_events=False&text=coding+programming+ruby+python+javascript+html&desc=False&offset=0&photo-host=public&format=json&lat=41.878114&page=200&lon=-87.629798&sig_id=178346822&sig=4dc9c0999b0508c2d262b1e2f21d355c21d04f26';
 
-function initialize() {
-  // http://stackoverflow.com/questions/6588549/make-google-maps-plugin-black-white-or-with-sepia-filter
-  var roadAtlasStyles = [
-    {
-      "featureType": "road.highway",
-      "elementType": "geometry",
-      "stylers": [
-        { "saturation": -100 },
-        { "lightness": -8 },
-        { "gamma": 1.18 }
-      ]
-    }, {
-      "featureType": "road.arterial",
-      "elementType": "geometry",
-      "stylers": [
-        { "saturation": -100 },
-        { "gamma": 1 },
-        { "lightness": -24 }
-      ]
-    }, {
-      "featureType": "poi",
-      "elementType": "geometry",
-      "stylers": [
-        { "saturation": -100 }
-      ]
-    }, {
-      "featureType": "administrative",
-      "stylers": [
-        { "saturation": -100 }
-      ]
-    }, {
-      "featureType": "transit",
-      "stylers": [
-        { "saturation": -100 }
-      ]
-    }, {
-      "featureType": "water",
-      "elementType": "geometry.fill",
-      "stylers": [
-        { "saturation": -100 }
-      ]
-    }, {
-      "featureType": "road",
-      "stylers": [
-        { "saturation": -100 }
-      ]
-    }, {
-      "featureType": "administrative",
-      "stylers": [
-        { "saturation": -100 }
-      ]
-    }, {
-      "featureType": "landscape",
-      "stylers": [
-        { "saturation": -100 }
-      ]
-    }, {
-      "featureType": "poi",
-      "stylers": [
-        { "saturation": -100 }
-      ]
+var meetup = {
+  "status": "upcoming",
+  "visibility": "public",
+  "maybe_rsvp_count": 0,
+  "venue": {
+    "id": 17723242,
+    "lon": -87.637566,
+    "repinned": false,
+    "name": "Dev Bootcamp",
+    "state": "IL",
+    "address_1": "351 W. Hubbard, Suite 700",
+    "lat": 41.88969,
+    "city": "Chicago",
+    "country": "us"
+  },
+  "id": "219671197",
+  "utc_offset": -21600000,
+  "distance": 0.8959337472915649,
+  "time": 1420759800000,
+  "waitlist_count": 0,
+  "updated": 1420654517000,
+  "yes_rsvp_count": 4,
+  "created": 1420654517000,
+  "event_url": "http://www.meetup.com/Chicago-DevBootcamp-Speaker-Series/events/219671197/",
+  "description": "<p>Interested in learning more about Dev Bootcamp? Are you a current student that is unsure about the job search or what it is like to work in the industry? Here is an event for you! Three DBC alumni that are all employed at Brad's Deals are going to form a panel for an \"Ask me Anything.\" Come ask questions about life before, during and after DBC!</p>",
+  "name": "DBCx: AMA with DBC Alumni",
+  "headcount": 0,
+  "group": {
+    "id": 11343122,
+    "created": 1386185186000,
+    "group_lat": 41.88999938964844,
+    "name": "DBCxCHI (Dev Bootcamp Chicago Speaker Series)",
+    "group_lon": -87.63999938964844,
+    "join_mode": "open",
+    "urlname": "Chicago-DevBootcamp-Speaker-Series",
+    "who": "Attendees"
+  }
+};
+
+String.prototype.titleize = function() {
+  var words = this.split(' ');
+  var array = [];
+  for (var i=0; i<words.length; ++i) {
+    array.push(words[i].charAt(0).toUpperCase() + words[i].slice(1));
+  }
+  return array.join(' ');
+};
+
+// corner model constructor, where `venueObject` == sub-item from Meetup API `open_venue` JSON response
+var Corner = function(venueObject) {
+  var self = this;
+
+  // load and check for latitude/longitude and set to `location`
+  self.lat = venueObject.lat;
+  self.lon = venueObject.lon;
+  self.location = ko.computed(function() {
+    if (self.lat === 0 || self.lon === 0) {
+      return null;
+    } else {
+      return new google.maps.LatLng(self.lat, self.lon);
     }
-  ];
+  });
 
-  var mapOptions = {
-    zoom: 14,
-    center: center,
-    mapTypeControlOptions: {
-      mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'usroadatlas']
-    },
+  // load metadata
+  self.id = ko.observable(venueObject.id);
+  self.name = ko.observable(venueObject.name.titleize());
+  self.address = ko.observable(venueObject.address_1);
 
-    // customize controls
-    mapTypeControl: false,
-    panControl: false,
-    streetViewControl: false,
-    zoomControlOptions: {
-      style: google.maps.ZoomControlStyle.SMALL
+  // initialize empty meetup
+  self.meetups = ko.observableArray([]);
+};
+
+// meetup model constructor, where `meetup` == item from Meetup API `open_venue` JSON response
+var Meetup = function(meetup) {
+  var self = this;
+
+  // attach venue object
+  self.venueObject = ko.observable(meetup.venue);
+  self.hasVenue = ko.computed(function() {
+    if (self.venueObject()) {
+      return true;
+    } else {
+      return false;
     }
-  };
-  map = new google.maps.Map($('#map-canvas')[0], mapOptions);
-  var styledMapOptions = {};
-  var usRoadMapType = new google.maps.StyledMapType(roadAtlasStyles, styledMapOptions);
-  map.mapTypes.set('usroadatlas', usRoadMapType);
-  map.setMapTypeId('usroadatlas');
-}
+  });
 
-google.maps.event.addDomListener(window, 'load', initialize);
+  // load metadata
+  self.id = ko.observable(meetup.id);
+  self.name = ko.observable(meetup.name.titleize());
+  self.group = ko.observable(meetup.group.name);
+  self.date = ko.computed(function() {
+    var milliseconds = meetup.time;
+    var date = new Date(milliseconds);
+    return date.toLocaleString();
+  });
+  self.url = ko.observable(meetup.event_url);
+};
+
+// main application View Model
+var ViewModel = function() {
+  var self = this;
+  var map;
+  var mapCanvas = $('#map-canvas')[0];
+  var center = new google.maps.LatLng(41.878114, -87.629798); // Chicago City Center
+
+  this.meetupList = ko.observableArray([]);
+  this.cornerList = ko.observableArray([]);
+  this.testMeetup = {'name': ko.observable('Loading...')};
+  this.testCorner = {'name': ko.observable('Loading...')};
+
+  function initialize() {
+    drawMap(center, mapCanvas);
+    fetchMeetups(meetupApiUrl);
+  }
+
+  // creates a black and white google map centered on 'center' onto the 'element' DOM element
+  function drawMap(center, element) {
+    // styling elements from http://stackoverflow.com/questions/6588549/make-google-maps-plugin-black-white-or-with-sepia-filter
+    var roadAtlasStyles = [
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          { "saturation": -100 },
+          { "lightness": -8 },
+          { "gamma": 1.18 }
+        ]
+      }, {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+          { "saturation": -100 },
+          { "gamma": 1 },
+          { "lightness": -24 }
+        ]
+      }, {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+          { "saturation": -100 }
+        ]
+      }, {
+        "featureType": "administrative",
+        "stylers": [
+          { "saturation": -100 }
+        ]
+      }, {
+        "featureType": "transit",
+        "stylers": [
+          { "saturation": -100 }
+        ]
+      }, {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+          { "saturation": -100 }
+        ]
+      }, {
+        "featureType": "road",
+        "stylers": [
+          { "saturation": -100 }
+        ]
+      }, {
+        "featureType": "administrative",
+        "stylers": [
+          { "saturation": -100 }
+        ]
+      }, {
+        "featureType": "landscape",
+        "stylers": [
+          { "saturation": -100 }
+        ]
+      }, {
+        "featureType": "poi",
+        "stylers": [
+          { "saturation": -100 }
+        ]
+      }
+    ];
+
+    var mapOptions = {
+      zoom: 14,
+      center: center,
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'usroadatlas']
+      },
+
+      // customize controls
+      mapTypeControl: false,
+      panControl: false,
+      streetViewControl: false,
+      zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.SMALL
+      }
+    };
+
+    map = new google.maps.Map(element, mapOptions);
+    var styledMapOptions = {};
+    var usRoadMapType = new google.maps.StyledMapType(roadAtlasStyles, styledMapOptions);
+    map.mapTypes.set('usroadatlas', usRoadMapType);
+    map.setMapTypeId('usroadatlas');
+  }
+
+  // create a map marker
+  function addMarker(corner) {
+    if (corner.location()) {
+      var marker = new google.maps.Marker({
+        position: corner.location(),
+        map: map,
+      });
+      google.maps.event.addListener(marker, 'click', function () {
+        console.log('Clicked!');
+      });
+    }
+  }
+
+  // fetch meetups via JSON-P from Meetup API
+  function fetchMeetups(url) {
+    var data;
+
+    // execute JSON-P request
+    $.ajax({
+      type: "GET",
+      url: url,
+      timeout: 5000,
+      contentType: "application/json",
+      dataType: "jsonp",
+      cache: false,
+
+    // when done
+    }).done(function(response) {
+      // pull `results` array from JSON
+      data = response.results;
+      console.log(data.length + ' meetups fetched...');
+
+      // loop through results and populate `meetupList`
+      data.forEach(function(meetup) {
+        self.meetupList.push(new Meetup(meetup));
+      });
+
+      // run the `extractCorners` function to pull location data
+      extractCorners();
+
+    // if failed
+    }).fail(function(response, status, error) {
+      // TODO: write error state to DOM
+      console.log(error);
+    });
+  }
+
+  // parse through `meetupList` and extract `Corner` objects
+  function extractCorners() {
+    // loop through meetup list
+    self.meetupList().forEach(function(meetup){
+      // check if meetup object has a valid venue id
+      if (meetup.hasVenue()) {
+        // attempt to fetch an existing corner, if it appears in the corner list
+        var corner = getCornerById(meetup.venueObject().id);
+
+        // if exists
+        if (corner) {
+          // push the meetup object onto the corner's meetups
+          corner.meetups().push(meetup);
+
+        // if does not exist
+        } else {
+          // instantiate a new corner object and push it to the corner list
+          corner = new Corner(meetup.venueObject());
+          self.cornerList().push(corner);
+
+          // and push the meetup object onto that new corner object
+          corner.meetups().push(meetup);
+
+          // add a marker
+          addMarker(corner);
+        }
+      }
+    });
+    console.log(self.cornerList().length + ' unique corners fetched...');
+  }
+
+  // checks if a specific corner already exists in `cornerList`
+  function getCornerById(id) {
+    self.cornerList().forEach(function(corner) {
+      if (corner.id === id) {
+        return corner;
+      }
+    });
+    return null;
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+};
+
+ko.applyBindings(new ViewModel());
