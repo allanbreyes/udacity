a8d97371e3a636834a7d0dca21b86606ac077af9
Allan Reyes <allanbreyes@gmail.com>
2015-01-09 19:28:37 -0600

    add commenting

--- js/application.js
+++ js/application.js
@@ -1,5 +1,8 @@
+// signed url from meetup api, taken from https://secure.meetup.com/meetup_api/console/?path=/2/open_events
 var meetupApiUrl = 'http://api.meetup.com/2/open_events?status=upcoming&radius=5&category=34&and_text=False&limited_events=False&text=coding+programming+ruby+python+javascript+html&desc=False&offset=0&photo-host=public&format=json&lat=41.878114&page=100&lon=-87.629798&sig_id=178346822&sig=1a186723262b63d4b2deee474b8d95bc0ec2ec9f';
 
+/* monkey patches for additional string functionality */
+// capitalizes first letter of each word if sentence is longer than 10 characters
 String.prototype.titleize = function() {
   var words = this.split(' ');
   var array = [];
@@ -16,6 +19,7 @@ String.prototype.titleize = function() {
   return array.join(' ');
 };
 
+// checks if a substring `other` is found inside the string
 String.prototype.contains = function(other) {
   return this.indexOf(other) !== -1;
 };
@@ -28,6 +32,8 @@ var Corner = function(venueObject) {
   self.lat = venueObject.lat;
   self.lon = venueObject.lon;
   self.location = ko.computed(function() {
+  // pseudo-model validation: if no lat/lon data, set location to null
+  // otherwise, instantiate a google maps coordinate
     if (self.lat === 0 || self.lon === 0) {
       return null;
     } else {
@@ -53,6 +59,8 @@ var Meetup = function(meetup) {
 
   // attach venue object
   self.venueObject = meetup.venue;
+
+  // returns if the meetup has a venue that's listed
   self.hasVenue = ko.computed(function() {
     if (self.venueObject) {
       return true;
@@ -65,6 +73,8 @@ var Meetup = function(meetup) {
   self.id = ko.observable(meetup.id);
   self.name = ko.observable(meetup.name.titleize());
   self.group = ko.observable(meetup.group.name);
+
+  // converts date in milliseconds to a human-friendly string, e.g. 1/2/2015
   self.date = ko.computed(function() {
     var milliseconds = meetup.time;
     var date = new Date(milliseconds);
@@ -76,45 +86,74 @@ var Meetup = function(meetup) {
 // main application View Model
 var ViewModel = function() {
   var self = this;
+
+  // future google map object, assigned in `drawMap`
   var map;
+
+  // target map canvas from DOM
   var mapCanvas = $('#map-canvas')[0];
-  var center = new google.maps.LatLng(41.878114, -87.629798); // Chicago City Center
+
+  // default center location, currently set to chicago
+  var center = new google.maps.LatLng(41.8886, -87.6310);
+
+  // google map marker tooltip
   var infoWindow = new google.maps.InfoWindow();
 
+  // list of meetups, not currently used in view
   self.meetupList = ko.observableArray([]);
+
+  // list of corners, bound to `#list`
   self.cornerList = ko.observableArray([]);
+
+  // number of corners, bound to `#search-summary p`
   self.numCorners = ko.observable(0);
 
   /* SEARCH */
 
-  // initialize search query to empty
+  // search query, bound to `#search-input` search box
+  // TODO: should be a computable
   self.query = ko.observable('');
 
+  // bound to `#search-btn`
   self.search = function() {
-    infoWindow.setMap(null);
+    // empty function for future functionality
+    // keep present to avoid page reload
   };
 
+  // returns a filtered list of corners if name contains `self.query` data
   self.filteredCornerList = ko.computed(function() {
+    // loop through corners and clear map markers
     self.cornerList().forEach(function(corner) {
       corner.marker.setMap(null);
     });
+
+    // filter results where name contains `self.query`
     var results = ko.utils.arrayFilter(self.cornerList(), function(corner) {
       return corner.name().toLowerCase().contains(self.query().toLowerCase());
     });
+
+    // go through results and set marker to visible
     results.forEach(function(corner) {
       corner.marker.setMap(map);
     });
+
+    // update the number of corners (couldn't get `ko.computed` to work)
     self.numCorners(results.length);
     return results;
   });
 
   /* SELECTION */
 
-  // initialize selected corner
-  self.selectedCorner = ko.observable();
-
+  // triggered when a corner in `#list` is clicked or a marker is clicked
   self.selectCorner = function(corner) {
-    self.selectedCorner(corner);
+
+    /* STUDENT NOTE: I was unable to get proper separation of concerns here, as I could not find
+     *  an intelligent way to use partials or layouts for Google Maps info windows.  I think that
+     *  this is also a security concern for injection attacks, and I would like feedback on how to
+     *  handle this.  Thank you!
+     */
+
+    // generate html for a corner's upcoming open meetups
     var formattedMeetupList = (function(corner) {
       result = '<ul class="info-window-list">';
       corner.meetups().forEach(function(meetup) {
@@ -122,7 +161,7 @@ var ViewModel = function() {
                   '<a href="' +
                   meetup.url() +
                   '">' +
-                  meetup.name() +
+                    meetup.name() +
                   '</a>' +
                   ' on ' +
                   meetup.date() +
@@ -131,14 +170,22 @@ var ViewModel = function() {
       result += '</ul>';
       return result;
     })(corner);
+    // generate additional html and set to info window content
     infoWindow.setContent('<div class="info-window-content">' +
                           '<span class="info-window-header">' +
                             corner.name() +
                           '</span>' +
+                          '<p>' +
+                            corner.address() +
+                          '</p>' +
                             formattedMeetupList +
                           '</div>'
                          );
+
+    // open up the appropriate info window at the selected corner's marker
     infoWindow.open(map, corner.marker);
+
+    // scroll the map to the marker's position
     map.panTo(corner.marker.position);
   };
 
@@ -220,13 +267,13 @@ var ViewModel = function() {
       mapTypeControl: false,
       panControl: false,
       streetViewControl: false,
-      zoomControl: false,
-      zoomControlOptions: {
-        style: google.maps.ZoomControlStyle.SMALL
-      }
+      zoomControl: false
     };
 
+    // assign a google maps element
     map = new google.maps.Map(element, mapOptions);
+
+    // apply custom map styling
     var styledMapOptions = {};
     var usRoadMapType = new google.maps.StyledMapType(roadAtlasStyles, styledMapOptions);
     map.mapTypes.set('usroadatlas', usRoadMapType);
@@ -236,15 +283,20 @@ var ViewModel = function() {
   // create a map marker
   function addMarker(corner) {
     var marker;
+
+    // validate that the corner has a location (see `Corner` model for pseudo-validation)
     if (corner.location()) {
       marker = new google.maps.Marker({
         position: corner.location(),
         map: map,
       });
+      // add a listener for when it's clicked
       google.maps.event.addListener(marker, 'click', function () {
         self.selectCorner(corner);
       });
     }
+
+    // return the marker object
     return marker;
   }
 
@@ -267,7 +319,6 @@ var ViewModel = function() {
     }).done(function(response) {
       // pull `results` array from JSON
       data = response.results;
-      console.log(data.length + ' meetups fetched...');
 
       // loop through results and populate `meetupList`
       data.forEach(function(meetup) {
@@ -279,8 +330,7 @@ var ViewModel = function() {
 
     // if failed
     }).fail(function(response, status, error) {
-      // TODO: write error state to DOM
-      console.log(error);
+      $('#search-summary').text('Meetup data could not load...');
     });
   }
 
@@ -318,10 +368,10 @@ var ViewModel = function() {
         }
       }
     });
-    console.log(self.cornerList().length + ' unique corners fetched...');
   }
 
   // checks if a specific corner by `id` already exists in `cornerList`
+  // TODO: check if they have the same lat/lon--but how to handle multiple floors?
   function hasCornerId(id) {
     var result = false;
     self.cornerList().forEach(function(corner) {
@@ -333,6 +383,7 @@ var ViewModel = function() {
   }
 
   // fetches a corner from `cornerList` by `id`
+  // TODO: check if they have the same lat/lon--but how to handle multiple floors?
   function getCornerById(id) {
     var foundCorner = null;
     if (hasCornerId(id)) {
