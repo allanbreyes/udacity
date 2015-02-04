567559aa45f23e23796b5583b5f7c5a6e31fc247
Allan Reyes <allanbreyes@gmail.com>
2015-02-04 02:31:30 -0600

    add movie import

--- p1/fresh_tomatoes.py
+++ p1/fresh_tomatoes.py
@@ -1,22 +1,24 @@
 import webbrowser
 import os
 import re
+import csv
 
 def read_template(template):
     """ returns html string from `templates/_<template>.html` """
     return ''.join([row for row in open('templates/_{}.html'.format(template),
                                         'r').readlines()])
 
-# Styles and scripting for the page
+# styles and scripting for the page
 main_page_head = read_template('head')
 
-# The main page layout and title bar
+# the main page layout and title bar
 main_page_content = read_template('content')
 
-# A single movie entry html template
+# a single movie entry html template
 movie_tile_content = read_template('movie')
 
 def create_movie_tiles_content(movies):
+    """ generates movie tile html content for a list of Movie objects """
     # The HTML content for this section of the page
     content = ''
     for movie in movies:
@@ -33,9 +35,10 @@ def create_movie_tiles_content(movies):
         )
     return content
 
-def open_movies_page(movies):
+def open_movies_page(movies, filename='fresh_tomatoes.html'):
+    """ creates output movie html and opens it in browser """
     # Create or overwrite the output file
-    output_file = open('fresh_tomatoes.html', 'w')
+    output_file = open(filename, 'w')
 
     # Replace the placeholder for the movie tiles with the actual dynamically generated content
     rendered_content = main_page_content.format(movie_tiles=create_movie_tiles_content(movies))
@@ -48,5 +51,31 @@ def open_movies_page(movies):
     url = os.path.abspath(output_file.name)
     webbrowser.open('file://' + url, new=2) # open in a new tab, if possible
 
+class Movie(object):
+    """ represents a movie """
+    def __init__(self, title, image_url, youtube_url):
+        self.title = title
+        self.poster_image_url = image_url
+        self.trailer_youtube_url = youtube_url
+
+    def __str__(self):
+        return self.title
+
+def get_movies(filename):
+    """ pulls movie data from csv and returns a list a Movie objects """
+    movies = []
+    with open(filename, 'rb') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter='\t')
+        for movie in reader:
+            movies.append(Movie(title=movie['name'],
+                                image_url=movie['image_url'],
+                                youtube_url=movie['youtube_url']))
+    return movies
+
+def main():
+    """ main subroutine """
+    movies = get_movies('data/fresh_tomatoes.csv')
+    open_movies_page(movies, 'fresh_tomatoes.html')
+
 if __name__ == '__main__':
-    open_movies_page([])
+    main()
